edef3ffeb058250cb60fc464b3a825b6c4bf80b0
Koga <kosloski.fkoga@gmail.com>
2020-12-24 14:14:30 -0300

    chore(backend): remove env variables from test function script

--- backend/scripts/test.js
+++ backend/scripts/test.js
@@ -3,14 +3,13 @@ const { execute, fileExists } = require('./utils');
 
 const functionName = process.argv[2];
 const fileName = process.argv[3];
-const enviromentVariables = process.argv[4];
 
-if (!functionName || !fileName || !enviromentVariables) {
+if (!functionName || !fileName) {
     process.exit(0);
 }
 
 const path = `functions/${functionName}/tests/${fileName}.json`;
 fileExists(path) &&
     execute(
-        `dotenv-load serverless invoke local --function ${functionName} --path ${path} ${enviromentVariables}`,
+        `dotenv-load serverless invoke local --function ${functionName} --path ${path}`,
     );
