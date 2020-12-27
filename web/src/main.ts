a709032ea53530d42d897599e16f913a2c9b5a57
Koga <kosloski.fkoga@gmail.com>
2020-12-27 18:16:00 -0300

    chore(web): configure amplify

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,13 +1,29 @@
+import { enableProdMode } from "@angular/core";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from "./app/app.module";
+import { environment } from "./environments/environment";
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import Amplify from "aws-amplify";
+import stack from "./stack.json";
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: stack.Region,
+    userPoolId: stack.UserPoolId,
+    identityPoolId: stack.IdentityPoolId,
+    identityPoolRegion: stack.Region,
+    userPoolWebClientId: stack.UserPoolClientId,
+  },
+  aws_appsync_region: stack.Region,
+  aws_appsync_authenticationType: "AMAZON_COGNITO_USER_POOLS",
+});
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
